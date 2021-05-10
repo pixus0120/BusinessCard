aae9ee5a70b29ba5ee7f045363f7a16837167674
pixus <pix_us@mail.ru>
2021-05-10 16:47:34 -0400

    Initial Commit

---
+++ Card/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Card
+//
+//  Created by locussigilli on 5/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
